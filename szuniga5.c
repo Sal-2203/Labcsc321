85160211586a9590c307d9c0d13b0975fc06c104
Sal-2203 <salzun11@gmail.com>
2025-02-19 01:21:45 +0000

    Lab5

---
+++ szuniga5.c
@@ -0,0 +1,36 @@
+//Saul Zuniga
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(void)
+{
+	int i = 0;
+	int a = 22;
+	float b = 555.5;
+	double c = 7.0;
+	char d = 'Z';
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+	printf("the value of a is %d and the address is %p \n", a,&a);
+	printf("the value of a is %.1f and the address is %p \n", b,&b);
+	printf("the value of a is %1f and the address is %p \n", c,&c);
+	printf("the value of a is %c and the address is %p \n", d,&d);
+
+	if(i; i<10; i++)
+	{
+		printf("adress of aar[%d] = %p \n", arr[i],&arr[i]);
+		int z = 1;
+	}
+	return 0;
+
+	printf("the value of a is %d and the address is %p \n",z,&z);
+
+	return EXIT_SUCCESS;
+}
+
+
