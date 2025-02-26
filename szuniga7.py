daf30bff2137aade1ea24248d8d2b4966a8b7e51
Sal-2203 <salzun11@gmail.com>
2025-02-26 02:22:37 +0000

    Lab 7

---
+++ szuniga7.py
@@ -0,0 +1,23 @@
+##Saul Zuniga
+
+
+
+x = 6
+w = "Celtics"
+c = 6.6
+
+
+if w == "Celtics":
+    print("Bakers, Makers, takers, Celtics in 5")
+else:
+    print("w is not california")
+
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than 5")
+
+if c > 5:
+    print("c is greater than 5")
+else:
+    print("c is less than 5")
