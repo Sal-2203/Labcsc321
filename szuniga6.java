b8997047f4cd83edd16c1cf1bc742b61269718a9
Sal-2203 <salzun11@gmail.com>
2025-02-26 00:29:51 +0000

    Lab 6

---
+++ szuniga6.java
@@ -0,0 +1,35 @@
+//Saul Zuniga Lab 5
+//
+//
+
+public class szuniga6
+{
+	public static void main(String[] args)
+	{
+		int x = 5;
+		int y =10;
+
+		double a =5;
+		double b = 10;
+
+		int m = x+y*x/y-x;
+
+		int n = -x-y/x*y+x;
+
+		int v = x+y-x/y;
+
+		double s = a+b*a/b-a;
+
+                double d = -a-b/a*b+a;
+
+                double f = a+b-a/b;
+
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(v);
+
+		System.out.println(s);
+                System.out.println(d);
+                System.out.println(f);
+	}
+}
