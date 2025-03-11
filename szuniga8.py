02bb1f4e9cabc02f006184aa71ec46995224a4dc
Sal-2203 <salzun11@gmail.com>
2025-03-11 23:28:31 +0000

    Lab 8

---
+++ szuniga8.py
@@ -0,0 +1,17 @@
+##Saul Zuniga
+
+x=7
+y=int(input("Enter a number 1-9: "))
+count=0
+
+if y==x:
+    print("You win")
+else:
+    print("Womp WOMp")
+
+for i in range(5):
+    print("Wanna hear a joke about salt?... Na")
+
+while count<7:
+    print("Katsu Chicken")
+    count+=1
